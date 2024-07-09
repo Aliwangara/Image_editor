8e12f547b339187a543c1d8842d91f559a0f13f0
Aliwangara <aliwangara63@gmail.com>
2024-07-09 23:47:26 +0300

    Update photoEditor.py
    Added functions for enhancing photo contrast

--- imageProcessor/photoEditor.py
+++ imageProcessor/photoEditor.py
@@ -2,10 +2,13 @@ from PIL import ImageEnhance,Image, ImageFilter
 import os
 
 path ='./imgs'
-pathOut = './EditedImgs'
+pathOut = '/EditedImgs'
 
 for filename in os.listdir(path):
     img = Image.open(f"{path} /{filename}")
-    edit = (ImageFilter.sharpen)
+    edit = (ImageFilter.SHARPEN).rotate(-90)
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit= enhancer(factor)
     clean_name = os.path.splitext(filename) [0]
     edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
