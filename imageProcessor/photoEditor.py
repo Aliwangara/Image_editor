297fc9ba8a73a2d77bb7d289bdfa58075928abc5
Aliwangara <aliwangara63@gmail.com>
2024-07-09 23:32:42 +0300

    Update photoEditor.py
    Added PathOut variable to store where images edited will be stored

--- imageProcessor/photoEditor.py
+++ imageProcessor/photoEditor.py
@@ -2,10 +2,10 @@ from PIL import ImageEnhance,Image, ImageFilter
 import os
 
 path ='./imgs'
-path = './EditedImgs'
+pathOut = './EditedImgs'
 
 for filename in os.listdir(path):
     img = Image.open(f"{path} /{filename}")
     edit = (ImageFilter.sharpen)
     clean_name = os.path.splitext(filename) [0]
-    edit.save{f'.{pathOut}/{clean_name}_edited.jpd'}
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
