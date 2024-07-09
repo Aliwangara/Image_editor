8cd61fd088d3f5e640f300dc1a8fcde3ced04888
Aliwangara <aliwangara63@gmail.com>
2024-07-09 23:29:11 +0300

    Update photoEditor.py
    added a python file for photoeditor

---
+++ imageProcessor/photoEditor.py
@@ -0,0 +1,11 @@
+from PIL import ImageEnhance,Image, ImageFilter
+import os
+
+path ='./imgs'
+path = './EditedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path} /{filename}")
+    edit = (ImageFilter.sharpen)
+    clean_name = os.path.splitext(filename) [0]
+    edit.save{f'.{pathOut}/{clean_name}_edited.jpd'}
